c55e6b56d17e52eb4ca2c356c1fb9797a6d07a46
Vivien MILLE <mille.vivien@proton.me>
2025-12-22 20:16:57 +0100

    pointer orientation dragscroll direction

--- keyboards/ploopyco/nano_2/rev2_003/keymaps/vmille/config.h
+++ keyboards/ploopyco/nano_2/rev2_003/keymaps/vmille/config.h
@@ -1,8 +1,5 @@
-/* Copyright 2021 Colin Lam (Ploopy Corporation)
-* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
- * Copyright 2019 Sunjun Kim
- * Copyright 2019 Hiroyuki Okada
- *
+/* Copyright 2024 Colin Lam (Ploopy Corporation)
+*
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -16,16 +13,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include QMK_KEYBOARD_H
 
-enum {
-    TD_KEY
-};
+#pragma once
+#define POINTING_DEVICE_ROTATION_90
 
-tap_dance_action_t tap_dance_actions[] = {
-   [TD_KEY] = ACTION_TAP_DANCE_DOUBLE(DRAG_SCROLL, KC_E)
-};
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( DRAG_SCROLL )
-};
+#undef PLOOPY_DRAGSCROLL_DIVISOR_H
+#define PLOOPY_DRAGSCROLL_DIVISOR_H -64.0
+#undef PLOOPY_DRAGSCROLL_DIVISOR_V
+#define PLOOPY_DRAGSCROLL_DIVISOR_V -64.0
