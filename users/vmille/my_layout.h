e6c9dac1ba89e7e7123dd3420916db3cf6ab8fb4
Vivien MILLE <mille.vivien@proton.me>
2025-12-29 20:34:27 +0100

    Correcting RGB & mouse change in QMK

--- users/vmille/my_layout.h
+++ users/vmille/my_layout.h
@@ -13,7 +13,7 @@
 #define ______________BASE_R2_LEFT______________ KC_BSLS, KC_Q           , KC_W        , KC_E        , KC_R        , KC_T
 #define ______________BASE_R3_LEFT______________ KC_GRV , LT(MOUSE, KC_A), LALT_T(KC_S), LSFT_T(KC_D), RCTL_T(KC_F), LCTL_T(KC_G)
 #define ______________BASE_R4_LEFT______________ KC_LBRC, KC_Z           , KC_X        , KC_C        , KC_V        , KC_B
-#define ______BASE_T_LEFT______ KC_ENT, KC_BTN1, LT(NAV, KC_DEL)
+#define ______BASE_T_LEFT______ KC_ENT, MS_BTN1, LT(NAV, KC_DEL)
 
 #define ______________BASE_R1_RIGHT______________ KC_6        , KC_7        , KC_8        , KC_9        , KC_0   , KC_MINS
 #define ______________BASE_R2_RIGHT______________ KC_Y        , KC_U        , KC_I        , KC_O        , KC_P   , KC_QUOT
@@ -34,9 +34,9 @@
 #define ______FUNCTION_T_RIGHT______ _______, _______, _______
 
 #define ______________NAV_R1_LEFT______________ KC_ESC , CG_LNRM,CG_LSWP, CG_RNRM,CG_RSWP, QK_BOOT
-#define ______________NAV_R2_LEFT______________ RGB_MOD , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ______________NAV_R3_LEFT______________ RGB_TOG , XXXXXXX, KC_LALT, KC_LSFT, KC_RCTL, KC_LCTL
-#define ______________NAV_R4_LEFT______________ RGB_RMOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ______________NAV_R2_LEFT______________ RM_NEXT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ______________NAV_R3_LEFT______________ RM_TOGG, XXXXXXX, KC_LALT, KC_LSFT, KC_RCTL, KC_LCTL
+#define ______________NAV_R4_LEFT______________ RM_PREV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 #define ______NAV_T_LEFT______ ENT_LCH, _______, _______
 
 #define ______________NAV_R1_RIGHT______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX , XXXXXXX, XXXXXXX
@@ -62,7 +62,7 @@
 #define ______________MOUSE_R2_LEFT______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 #define ______________MOUSE_R3_LEFT______________ XXXXXXX, XXXXXXX, DRGSCRL, SNIPING, XXXXXXX, XXXXXXX
 #define ______________MOUSE_R4_LEFT______________ XXXXXXX, XXXXXXX, C(KC_X), C(KC_C), C(KC_V), XXXXXXX
-#define ______MOUSE_T_LEFT______ KC_BTN2, KC_BTN1, KC_BTN3
+#define ______MOUSE_T_LEFT______ MS_BTN2, MS_BTN1, MS_BTN3
 
 #define ______________MOUSE_R1_RIGHT______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 #define ______________MOUSE_R2_RIGHT______________ KC_WBAK, KC_WFWD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
